3ab6e63d74c251bfb3c06f1015b34418142214ff
bluesmi <mi19961212@outlook.com>
2017-06-29 20:02:15 +0800

    把图片路径改小了

--- app/src/main/java/com/penglai/kjds/ui/my/ClipImageActivity.java
+++ app/src/main/java/com/penglai/kjds/ui/my/ClipImageActivity.java
@@ -90,7 +90,7 @@ public class ClipImageActivity extends BaseActivity {
             Log.e("android", "zoomedCropBitmap == null");
             return;
         }
-        Uri mSaveUri = Uri.fromFile(new File(getCacheDir(), "cropped_1.jpg"));
+        Uri mSaveUri = Uri.fromFile(new File(getCacheDir(), "1.jpg"));
         if (mSaveUri != null) {
             OutputStream outputStream = null;
             try {
